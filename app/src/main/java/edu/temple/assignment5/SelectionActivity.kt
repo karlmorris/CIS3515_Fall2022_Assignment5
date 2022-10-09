5b405695fed8cfd4725d2d1feee3c932691d242f
Karl Morris <karl.morris@temple.edu>
2022-10-09 11:39:31 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/assignment5/SelectionActivity.kt
@@ -0,0 +1,11 @@
+package edu.temple.assignment5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
